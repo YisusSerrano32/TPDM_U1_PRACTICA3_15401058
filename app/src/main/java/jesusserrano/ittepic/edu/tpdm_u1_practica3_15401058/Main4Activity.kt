f2f60fb52878a6b19ecd6fdddd25e13f578015fd
YisusSerrano32 <jeedserranode@ittepic.edu.mx>
2019-09-24 20:21:22 -0600

    TPDM_U1_PRACTICA3_15401058

---
+++ app/src/main/java/jesusserrano/ittepic/edu/tpdm_u1_practica3_15401058/Main4Activity.kt
@@ -0,0 +1,12 @@
+package jesusserrano.ittepic.edu.tpdm_u1_practica3_15401058
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main4Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+    }
+}
